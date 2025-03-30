afb77d749a25c56938d95f01d480cea7e058a051
rave-roses <odekunlewaris01@gmail.com>
2025-03-30 19:31:57 +0100

    added

--- dist/index.js
+++ dist/index.js
@@ -1,1 +1,33 @@
 "use strict";
+let positionInformation = [
+    "Your info ",
+    "Select plan ",
+    "Add-ons",
+    "Summary",
+];
+let information = document.querySelector(".position-information");
+let unorderedList = document.createElement("ul");
+unorderedList.className = "unordered";
+unorderedList.style.listStyleType = "none";
+information === null || information === void 0 ? void 0 : information.appendChild(unorderedList);
+if (Array.isArray(positionInformation)) {
+    positionInformation.map((element, index) => {
+        let list = document.createElement("li");
+        let numberDiv = document.createElement("div");
+        numberDiv.className = "number";
+        const checkNumber = numberDiv ? String(index + 1) : "";
+        numberDiv.textContent = checkNumber;
+        let infoDiv = document.createElement("div");
+        let firstsubDiv = document.createElement("div");
+        firstsubDiv.className = "step";
+        firstsubDiv.textContent = `STEP ${index + 1}`;
+        let secondsubDiv = document.createElement("div");
+        secondsubDiv.className = "secondsubDiv";
+        secondsubDiv.textContent = element.toUpperCase();
+        unorderedList === null || unorderedList === void 0 ? void 0 : unorderedList.appendChild(list);
+        list.appendChild(numberDiv);
+        list.appendChild(infoDiv);
+        infoDiv.appendChild(firstsubDiv);
+        infoDiv.appendChild(secondsubDiv);
+    });
+}
---
+++ src/index.ts
@@ -0,0 +1,34 @@
+let positionInformation: string[] = [
+  "Your info ",
+  "Select plan ",
+  "Add-ons",
+  "Summary",
+];
+
+let information = document.querySelector(".position-information");
+let unorderedList = document.createElement("ul");
+unorderedList.className = "unordered";
+unorderedList.style.listStyleType = "none";
+information?.appendChild(unorderedList);
+
+if (Array.isArray(positionInformation)) {
+  positionInformation.map((element, index) => {
+    let list = document.createElement("li");
+    let numberDiv = document.createElement("div");
+    numberDiv.className = "number";
+    const checkNumber = numberDiv ? String(index + 1) : "";
+    numberDiv.textContent = checkNumber;
+    let infoDiv = document.createElement("div");
+    let firstsubDiv = document.createElement("div");
+    firstsubDiv.className = "step";
+    firstsubDiv.textContent = `STEP ${index + 1}`;
+    let secondsubDiv = document.createElement("div");
+    secondsubDiv.className = "secondsubDiv";
+    secondsubDiv.textContent = element.toUpperCase();
+    unorderedList?.appendChild(list);
+    list.appendChild(numberDiv);
+    list.appendChild(infoDiv);
+    infoDiv.appendChild(firstsubDiv);
+    infoDiv.appendChild(secondsubDiv);
+  });
+}
